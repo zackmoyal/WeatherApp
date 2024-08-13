9288d3e0896d69fd8aacc9e8003d0571a61ce3bc
Zack Moyal <zack.moyal@gmail.com>
2024-08-13 06:04:12 -0700

    working weather api call, finished result

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace WeatherApp
+﻿using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			var client = new HttpClient();
+
+			var key = "6deab23cecd72a05bd988eb02a5be361";
+
+			while (true)
+			{
+                Console.WriteLine();
+                Console.WriteLine("Please enter the city name: ");
+				var city_name = Console.ReadLine();
+                Console.WriteLine();
+
+				var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={key}";
+
+				try
+				{
+					var response = client.GetAsync(weatherURL).Result;
+					var responseContent = response.Content.ReadAsStringAsync().Result;
+
+					if (responseContent != null)
+					{
+						var formattedResponse = JObject.Parse(responseContent).GetValue("main")?.ToString();
+						var temp = JObject.Parse(formattedResponse).GetValue("temp");
+						Console.WriteLine($"The current Temperature is {temp} degrees Fahrenheit");
+					}
+					
+				}
+				catch (Exception e)
+				{
+                    Console.WriteLine(e.Message);
+                }
+
+				AddSpaces(2);
+				Console.WriteLine("Would you like to exit?");
+				var userInput = Console.ReadLine();
+				AddSpaces(2);
+
+				if (userInput.ToLower().Trim() == "yes")
+				{
+					break;
+				}
+			}
+        }
+
+		private static void AddSpaces(int count)
+		{
+			for (int i = 0; i < count; i++)
+			{
+                Console.WriteLine();
+            }
 		}
 	}
 }
--- WeatherApp/obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11bd84c8ad909bb2e4f10309e2db3c22973d785")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
